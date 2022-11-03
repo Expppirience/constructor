70812b7985557df5a29abc58e1d5ecc07d3e34fe
Expppirience <forakk01@yandex.ru>
2022-11-03 09:42:19 +0300

    initial markup & styles added

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener('load')
+
+const model = [
+  { type: "title", value: "JS placeholder" },
+  { type: "text", value: "text placeholder" },
+  {
+    type: "columns",
+    value: ["123", "321", "231"],
+  },
+];
